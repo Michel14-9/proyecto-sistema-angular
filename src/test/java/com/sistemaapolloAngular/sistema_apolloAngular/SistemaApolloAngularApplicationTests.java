be8271a841f2b0262ee08d70f943cafc5995b4e9
Michel14-9 <duval.14-99@hotmail.com>
2026-04-14 10:32:01 -0500

    Backend Spring Boot - Sistema Apollo

---
+++ src/test/java/com/sistemaapolloAngular/sistema_apolloAngular/SistemaApolloAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaapolloAngular.sistema_apolloAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApolloAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
