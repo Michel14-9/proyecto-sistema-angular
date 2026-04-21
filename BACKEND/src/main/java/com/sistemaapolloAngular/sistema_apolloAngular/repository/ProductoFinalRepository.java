a54a48752302f4fd158cbda3ace5ba0774784080
Anthony Varillas Sánchez <varillassanchezanthony@gmail.com>
2026-04-20 20:42:55 -0500

    AVANCE DE LAS VISTAS

--- BACKEND/src/main/java/com/sistemaapolloAngular/sistema_apolloAngular/repository/ProductoFinalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistemaapolloAngular.sistema_apolloAngular.repository;
-
-import com.sistemaapolloAngular.sistema_apolloAngular.model.ProductoFinal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoFinalRepository extends JpaRepository<ProductoFinal, Long> {
-}
