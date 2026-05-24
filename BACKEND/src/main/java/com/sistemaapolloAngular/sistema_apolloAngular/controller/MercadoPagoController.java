4f5dd5fa51f79087e79f234d2a83851cfae9a1cf
Michel14-9 <duval.14-99@hotmail.com>
2026-05-24 18:55:38 -0500

    controller webhook

--- BACKEND/src/main/java/com/sistemaapolloAngular/sistema_apolloAngular/controller/MercadoPagoController.java
+++ BACKEND/src/main/java/com/sistemaapolloAngular/sistema_apolloAngular/controller/MercadoPagoController.java
@@ -3,6 +3,7 @@ package com.sistemaapolloAngular.sistema_apolloAngular.controller;
 import com.sistemaapolloAngular.sistema_apolloAngular.dto.PagoRequest;
 import com.sistemaapolloAngular.sistema_apolloAngular.service.PagoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,5 +17,11 @@ public class MercadoPagoController {
     public String crearPago(@RequestBody PagoRequest request) throws Exception {
 
         return pagoService.crearPago(request);
+    }
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> recibirNotificacion(@RequestBody String notification) {
+        System.out.println("Notificación MP: " + notification);
+        // Aquí actualizas el estado del pedido en tu base de datos
+        return ResponseEntity.ok().build();
     }
 }
